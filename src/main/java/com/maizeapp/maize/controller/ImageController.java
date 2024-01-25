5a6e5dcd4871c2aa352d502d8c4c324dffaafb6f
ShaliniEdara <edara.shalini@gmail.com>
2024-01-26 03:30:56 +0530

    apis for image controller

---
+++ src/main/java/com/maizeapp/maize/controller/ImageController.java
@@ -0,0 +1,74 @@
+package com.maizeapp.maize.controller;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.maizeapp.maize.builder.ImageBuilder;
+import com.maizeapp.maize.entity.Image;
+import com.maizeapp.maize.entity.User;
+import com.maizeapp.maize.repository.UserRepository;
+import com.maizeapp.maize.service.ImageService;
+
+@RestController
+@RequestMapping(value ="/image")
+public class ImageController {
+	@Autowired
+	private ImageService imageService;
+	@Autowired
+	private ImageBuilder imageBuilder;
+    @Autowired
+    private UserRepository userRepository;
+	
+	@RequestMapping(value = "/{id}/uploadimage",method = RequestMethod.POST)
+    public String uploadImage(@RequestParam("file") MultipartFile file,@PathVariable("id")Long id,@RequestBody Image image) {
+        try {
+            // Convert MultipartFile to byte[]
+            byte[] imageData = file.getBytes();
+           Optional<User> user= userRepository.findById(id);
+           if(user.isPresent()) {
+        	   User userId =user.get();
+
+            // Create Image entity and set image data
+            Image newImage = new Image();
+            newImage.setName(file.getOriginalFilename());
+            //newImage.setPath("/path/to/images"); // Set the path as needed
+            //newImage.setImageData(imageData);
+            newImage.setHeight(image.getHeight());
+            newImage.setWidth(image.getWidth());
+            newImage.setSize(image.getSize());
+            newImage.setUser(userRepository.findByUser(userId));
+
+            // Save image
+            imageService.saveImage(image);
+           }
+
+            return "Image uploaded successfully!";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Plz give proper file for image loag";
+    }
+} 
+	
+	
+	@RequestMapping(value="/{userid}/imagelist",method = RequestMethod.GET)
+	private List<Image> ImageList(@PathVariable("userid")Long userid){
+		System.out.println("ytyeuydeudyuwydw");
+		System.out.println("userid is "+userid);
+		return imageService.imageList(userid);
+		
+	}
+}
+
+
---
+++ src/main/java/com/maizeapp/maize/serviceimpl/ImageServiceImpl.java
@@ -0,0 +1,50 @@
+package com.maizeapp.maize.serviceimpl;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maizeapp.maize.builder.ImageBuilder;
+import com.maizeapp.maize.entity.Image;
+import com.maizeapp.maize.entity.User;
+import com.maizeapp.maize.repository.ImageRepository;
+import com.maizeapp.maize.repository.UserRepository;
+import com.maizeapp.maize.service.ImageService;
+
+@Service
+public class ImageServiceImpl implements ImageService {
+	@Autowired
+	private ImageService imageService;
+	@Autowired 
+	private ImageBuilder imageBuilder;
+	@Autowired 
+	private UserRepository userRepository;
+	@Autowired
+	private  ImageRepository imageRepository;
+	@Override
+	public String saveImage(Image image) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<Image> imageList(Long userid) {
+		Optional<User> userOptional = userRepository.findById(userid);
+
+	    if (userOptional.isPresent()) {
+	        User user = userOptional.get();
+	        List<Image> images = imageRepository.findByUser(user);
+	        return images;
+	    } else {
+	        System.out.println("User with ID " + userid + " not found.");
+	        return Collections.emptyList(); 
+	    }
+
+	}
+	
+	
+	
+
+}
