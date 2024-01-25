119ac4e936b4b1a129a5da5b56bc95091813e040
thanuja105 <v.thanuja105@gmail.com>
2024-01-25 22:47:25 +0530

    Address builder code....

---
+++ src/main/java/com/maizeapp/maize/builder/AddressBuilder.java
@@ -0,0 +1,45 @@
+package com.maizeapp.maize.builder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.maizeapp.maize.dto.request.AddressRequest;
+import com.maizeapp.maize.dto.response.AddressResponse;
+import com.maizeapp.maize.dto.response.ImageResponse;
+import com.maizeapp.maize.entity.Address;
+import com.maizeapp.maize.entity.Image;
+
+
+@Component
+public class AddressBuilder {
+	
+     public Address toModel(AddressRequest addressRequest) {
+		
+	    Address address=new Address();
+	    address.setCity(addressRequest.getCity());
+	    address.setState(addressRequest.getState());
+	    return address;
+    }
+
+     public AddressResponse toDto(Address address) {
+ 		AddressResponse addressResponse =new AddressResponse();
+ 		addressResponse.setId(address.getId());
+ 		addressResponse.setCity(address.getCity());
+ 		addressResponse.setState(address.getState());
+ 		return addressResponse;
+ 		
+     }
+     
+     public List<AddressResponse> toDoList(List<Address> address){
+ 		List<AddressResponse> addressResponse = new ArrayList<AddressResponse>();
+ 		for (Address addressInfo : address) {
+ 			addressResponse.add(toDto(addressInfo));
+ 		}
+ 		
+ 		return addressResponse;
+ 	}
+
+
+}
