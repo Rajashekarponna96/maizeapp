7bf90fbdf8499e880bb93ac60ac9e8e72c2faf7a
rajashekarponna96 <rajashekar.ponna96@gmail.com>
2024-01-25 12:07:54 +0530

    initial commit

---
+++ src/main/java/com/maizeapp/maize/MaizeappApplication.java
@@ -0,0 +1,13 @@
+package com.maizeapp.maize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaizeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaizeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maizeapp/maize/MaizeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maizeapp.maize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaizeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
